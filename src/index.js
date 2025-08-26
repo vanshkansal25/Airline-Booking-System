3d1be787a9b0016edf8f2b118ecc2183bcfdd357
@vanshkansal25 <kansalv578@gmail.com>
2025-08-26 22:05:05 +0530

    Add Winston logger and refactor server configuration
    - Integrated Winston for logging functionality.
    - Created separate logger and server configuration files.
    - Updated index.js to utilize the new configuration structure.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const {PORT} = require('./config');
+const {ServerConfig  } = require('./config');
 const apiRoutes = require('./routes')
 const app = express();
 
 app.use("/api",apiRoutes);
 
-app.listen(PORT,()=>{
+app.listen(ServerConfig.PORT,()=>{
     console.log(`Successfully listening at Port ${PORT}`)
 })
 
